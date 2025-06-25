846efb2efdacc53f3e404675cbf07306781e2d8b
amjad <amjadtadmory@gmail.com>
2025-06-25 12:35:29 +0300

    a

--- app.js
+++ app.js
@@ -16,33 +16,33 @@ const app = express()
 
 // app.enable('trust proxy');
 
-// if (process.env.NODE_ENV === 'development') {
-//     app.use(morgan('dev'))
-// }
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
-// app.use(cors({
-//     origin: process.env.ORIGIN
-// }));
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
 
-// app.use(helmet());
+app.use(helmet());
 
-// const limiter = rateLimit({
-//     max: 100,
-//     windowMs: 60 * 60 * 1000,
-//     message: 'Too many requests from this IP, please try again in an hour!'
-// });
-// app.use('/api', limiter);
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
-// app.use(express.json({ limit: '10kb' }));
-// app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-// app.use(cookieParser());
-// app.use(compression());
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+app.use(compression());
 
 
-// app.use('/api/v1/users', userRouter)
-// app.use('/api/v1/orders', protect, orderRouter)
-// app.use('/api/v1/products', protect, productRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/orders', protect, orderRouter)
+app.use('/api/v1/products', protect, productRouter)
 
-// app.use(globalErrorHandler)
+app.use(globalErrorHandler)
 
 module.exports = app
